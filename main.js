a37b20ccd2abd4800049bf232836c36280af4b38
Cosbot <cosbot@Cosbots-MacBook-Pro.local>
2019-10-06 15:17:21 +1000

    data types in Javascript

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log('Hello World');
-console.error('This is an error');
-console.warn('This is a warning');
+// var, let, const
+// let and const are NOT interchangable, but use const as it is stronger
+
+//Data Types: "String", Numbers, Boolean (true/false), Null (empty), Undefined, Symbol
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+
+console.log(typeof rating);
