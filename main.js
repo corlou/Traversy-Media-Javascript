728769fd429e151fe42c340482922fdadda43ee9
Cosbot <cosbot@Cosbots-MacBook-Pro.local>
2019-10-06 16:12:20 +1000

    arrays

--- main.js
+++ main.js
@@ -3,10 +3,24 @@
 
 //Data Types: "String", Numbers, Boolean (true/false), Null (empty), Undefined, Symbol
 
-const name = "John";
+/*const name = "John";
 const age = 30;
 
 //Concatenation
 console.log("My name is " + name + " and I am " + age)
 //Template String
-console.log(`My name is ${name} and I am ${age}`);
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);*/
+
+//Arrays  - variables that hold multiple values
+
+const fruits = ["apples", "oranges", "pears"];
+
+fruits[3] = "grapes";
+
+fruits.push("mangos");
+
+fruits.unshift("strawberries");
+
+console.log(fruits);
